8704931b0e7fba7d834ead809b617a8bf1b391f6
ellielle <40385743+ellielle@users.noreply.github.com>
2023-09-15 18:30:26 -0400

    add first 2 challenges from 03

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,11 @@
-function sumOfEvenSquares() {}
+/**
+ * Returns the sum of the squares of the even numbers in the array.
+ * @param {number[]} numbers - The array of numbers.
+ * @returns {number} - The sum of the squares of even numbers.
+ */
+function sumOfEvenSquares(numbers) {
+  const evenSquares = numbers.filter((num) => num % 2 === 0).map((num) => num ** 2);
+  return evenSquares.reduce((prev, curr) => prev + curr, 0);
+}
 
 module.exports = sumOfEvenSquares;
--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,22 @@
-function calculateTotalSalesWithTax() {}
+/**
+ * Calculates and returns the total sales amount including tax from the input array of products and tax rate.
+ * @param {Object[]} products - The array of product objects.
+ * @param {string} products[].name - The name of the product.
+ * @param {number} products[].price - The price of the product.
+ * @param {number} products[].quantity - The quantity sold of the product.
+ * @param {number} taxRate - The tax rate as a percentage.
+ * @returns {number} - The total sales amount including tax.
+ */
+function calculateTotalSalesWithTax(products, taxRate) {
+  let totalSales = 0;
+  
+  products.forEach(({price, quantity}) => {
+    totalSales += (quantity * price);
+  })
+
+  const salesTax = totalSales * (taxRate / 100);
+
+  return totalSales + salesTax;
+}
 
 module.exports = calculateTotalSalesWithTax;
