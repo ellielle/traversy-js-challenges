a289a5fb280896d9edb6bf21d81bce3d31026b3e
ellielle <40385743+ellielle@users.noreply.github.com>
2023-09-29 09:37:31 -0400

    (╯°□°)╯︵ ┻━┻

--- 04-recursion/09-flatten-array/flatten-array.js
+++ 04-recursion/09-flatten-array/flatten-array.js
@@ -1,3 +1,18 @@
-function flattenArray() {}
+/**
+ * Returns a flattened array.
+ * @param {number[]} arr - The array containing nested arrays.
+ * @returns {number[]} - The flattened array.
+ */
+function flattenArray(arr) {
+  let flattened = [];
+  for (const element of arr) {
+    if (!Array.isArray(element)) {
+      flattened.push(element);
+    } else {      
+      flattened = flattened.concat(flattenArray(element))
+    }
+  }
+  return flattened
+}
 
 module.exports = flattenArray;
--- 04-recursion/10-permutations/permutations.js
+++ 04-recursion/10-permutations/permutations.js
@@ -1,3 +1,25 @@
-function permutations() {}
+/**
+ * Returns all possible permutations of the characters in a string.
+ * @param {string} str - The string to permute.
+ * @returns {string[]} - An array of all possible permutations of the characters in the string.
+ */
+function permutations(str) {
+  const resultArr = [];
+  if (str.length === 0) {
+    resultArr.push("");
+    return resultArr;
+  }
+  for (let i = 0; i < str.length; i++) {
+    const firstLetter = str.at(i);
+    const newStr = str.slice(0, i) + str.slice(i + 1);
+    const letterPermutations = permutations(newStr);
+
+    for (let j = 0; j < letterPermutations.length; j++) {
+      resultArr.push(`${firstLetter}${letterPermutations[j]}`);
+    }
+  }
+
+  return resultArr;
+}
 
 module.exports = permutations;
